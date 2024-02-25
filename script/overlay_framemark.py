0aab2e412da00dc27af9745eb0ff32980ee18e23
Seung Hyun Kim <skim449@illinois.edu>
2024-02-24 21:56:47 -0600

    feat: add script to imprint frame number and time on a video
    This commit adds a new script that uses FFmpeg to imprint the frame number and time on a video. The script takes in a video file path and an output file path as command line arguments. It uses the `drawtext` filter of FFmpeg to add the frame number and time as text on each frame of the video. The resulting video is saved to the specified output file path. The script also includes options for verbose mode and dry run.

---
+++ script/overlay_framemark.py
@@ -0,0 +1,56 @@
+import subprocess
+import os, sys
+import pathlib
+
+import click
+
+
+def extract_video_with_framemark(input_path: str, output_path: str):
+    # mark frame number and time
+    command = ["ffmpeg", "-y"]
+    command.extend(["-i", f"{input_path}"])
+    command.extend(
+        [
+            "-vf",
+            "drawtext=text='Frame %{n}, Time\: %{pts\:gmtime\:0\:%H\\\:%M\\\:%S}':fontsize=30:fontcolor=white:borderw=2:bordercolor=black:x=10:y=h-th-10",
+        ]
+    )
+    command.extend(["-c:a", "copy"])
+    command.extend([f"{output_path}"])
+    command = " ".join(command)
+    print("running : ", command)
+
+    sts = subprocess.Popen(command, shell=True).wait()
+    return sts
+
+
+@click.command()
+@click.option(
+    "-f",
+    "--filepath",
+    type=str,
+    help="video file path",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=str,
+    help="output file path",
+)
+@click.option("-v", "--verbose", is_flag=True, help="Verbose mode.")
+@click.option("-d", "--dry", is_flag=True, help="Dry run.")
+def process(filepath, output, verbose: bool, dry: bool):
+    """
+    Imprint frame number and time on the video
+
+    Parameters
+    ----------
+    path: str
+        Video path
+    """
+    assert os.path.exists(filepath)
+    extract_video_with_framemark(filepath, output)
+
+
+if __name__ == "__main__":
+    process()
